9723cabeb7ad7bfea77e1d319e428df992ebcc57
Yasin440 <eiasin440@gmail.com>
2022-01-19 23:50:32 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './shared/Header/Header';
+import Home from './components/Home/Home';
+import Register from './components/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ToastContainer position='top-center' />
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/shared/Header/Header.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+import DehazeIcon from '@mui/icons-material/Dehaze';
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    const [toggle, setToggle] = useState(false);
+    return (
+        <div className='header'>
+            <div className="nav-bar">
+                <p className="logo">My Subject</p>
+                <div className={`nav-right ${toggle ? 'toggleBtnActive' : ""}`}>
+                    <NavLink
+                        style={({ isActive }) => {
+                            return {
+                                backgroundColor: isActive ? "#362682" : "",
+                                color: isActive ? "white" : ""
+                            };
+                        }}
+                        to='/home'
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => {
+                            return {
+                                backgroundColor: isActive ? "#362682" : "",
+                                color: isActive ? "white" : ""
+                            };
+                        }}
+                        to='/addSubject'
+                    >
+                        Add Subject
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => {
+                            return {
+                                backgroundColor: isActive ? "#362682" : "",
+                                color: isActive ? "white" : ""
+                            };
+                        }}
+                        to='/about'
+                    >
+                        About
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => {
+                            return {
+                                backgroundColor: isActive ? "#362682" : "",
+                                color: isActive ? "white" : ""
+                            };
+                        }}
+                        to='/contactUS'
+                    >
+                        Contact Us
+                    </NavLink>
+                    <NavLink className='registerBtn' to='/register'>
+                        Register
+                    </NavLink>
+                </div>
+                <div className="toggleBtn">
+                    <Button onClick={() => setToggle(!toggle)} className="toggleBtn" sx={{ color: 'brown', cursor: 'pointer' }}><DehazeIcon /></Button>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Header;
